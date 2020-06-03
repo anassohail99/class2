847be8ee5405ca59363ac50c0eee79061912421e
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-04 00:05:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <div className="App">
+      <h1>HELLO WORDL FROM ANAS</h1>
+    </div>
+  );
 }
 
 export default App;
